2d5223be114b59909be76eed57c93f356c3f4ce5
izmirtais <izmirtais28@gmail.com>
2019-09-30 16:31:16 +0700

    mengerjakan praktikum Listener

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -8,17 +8,32 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
+import id.ac.polinema.idealbodyweight.fragments.BrocaIndexFragment;
+import id.ac.polinema.idealbodyweight.fragments.MenuFragment;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener,
+        BrocaIndexFragment.OnFragmentInteractionListener {
+
+    private BrocaIndexFragment brocaIndexFragment;
 
 	// Deklarasikan atribut Fragment di sini
 	private AboutFragment aboutFragment;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		aboutFragment = AboutFragment.newInstance("Izmir Tais Khasna Lugazi");
+
+		MenuFragment menuFragment = new MenuFragment();
+
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, menuFragment)
+				.commit();
+
+        brocaIndexFragment = new BrocaIndexFragment();
 	}
 
 	@Override
@@ -38,4 +53,21 @@ public class MainActivity extends AppCompatActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+    @Override
+    public void onBrocaIndexButtonClicked() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, brocaIndexFragment)
+                .commit();
+    }
+
+	@Override
+	public void onBodyMassIndexButtonClicked() {
+
+	}
+
+    @Override
+    public void onCalculateBrocaIndexClicked(float index) {
+
+    }
 }
